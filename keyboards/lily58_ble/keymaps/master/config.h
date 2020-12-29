75306a3a3a25a8b1d7b482e9a0c9a3afd1a8c7c5
ASHIJANKEN <tortoise.mountain.3408@gmail.com>
2020-12-29 22:01:39 +0900

    Add lily58_ble

---
+++ keyboards/lily58_ble/keymaps/master/config.h
@@ -0,0 +1,35 @@
+/* Copyright 2019 sekigon-gonnoc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+#define IS_LEFT_HAND true
+#define BMP_DEFAULT_MODE SPLIT_MASTER
+
+// #define SSD1306OLED
+
+#define USE_SERIAL_PD2
+
+#define TAPPING_FORCE_HOLD
+
+// place overrides here
